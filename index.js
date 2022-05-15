6ece5c1a4e322e3dee1d848955a978fb54e93b92
Maria Korneeva <korneevamg@gmail.com>
2022-05-15 13:24:48 +0200

    add flex

--- index.js
+++ index.js
@@ -147,6 +147,12 @@ new Promise(resolve => {
  */
 // Set the name of the hidden property and the change event for visibility
 var hidden, visibilityChange;
+var visibilityChangeEnabled = false;
+
+function toggleVisibilityChange() {
+    visibilityChangeEnabled = !visibilityChangeEnabled;
+}
+
 if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
     hidden = "hidden";
     visibilityChange = "visibilitychange";
@@ -166,7 +172,7 @@ function handleVisibilityChange() {
         // https://developers.google.com/youtube/iframe_api_reference
         videoElement.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
 
-    } else {
+    } else if (visibilityChangeEnabled) {
         videoElement.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
     }
 }
